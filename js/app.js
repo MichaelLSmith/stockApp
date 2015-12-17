149016dfe8a6fc1811ce6245dbe51b68f03e009a
MichaelLSmith <mlsmith45@gmail.com>
2015-12-17 10:51:38 -0500

    with both xAxis and Yaxis showing

--- js/app.js
+++ js/app.js
@@ -7,58 +7,57 @@ console.log(data);
 
 var outerWidth = 500;
     outerHeight = 250;
+    buffer = 10;
     margin = {left: 30, top: 30, right: 30, bottom: 30};
 
 var innerWidth  = outerWidth  - margin.left - margin.right;
-console.log('innerWidth');
-console.log(innerWidth);
-
 var innerHeight = outerHeight - margin.top  - margin.bottom;
-console.log('innerHeight');
-console.log(innerHeight);
 
 
 var svg = d3.select("body").append("svg")
     .attr("width", outerWidth)
     .attr("height", outerHeight);
+
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 var path = g.append("path");
-var axis = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + 230 + ")");
 
+//Axes function definitions
+var xAxisG = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + (outerHeight - margin.bottom ) + ")");
 
+var yAxisG = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top  + ")");
+
+//Scale creation
 var xScale = d3.scale.linear().range([0, innerWidth]);
 var yScale = d3.scale.linear().range([innerHeight, 0]);
 
+//Line creation
 var line = d3.svg.line()
     .x(function (d) {return xScale(d[0]); })
     .y(function (d) {return yScale(d[1]); });
 
 // create yAxis
     var xAxis = d3.svg.axis()
-                  .scale(xScale)
-                  .orient("bottom");
+        .scale(xScale)
+        .orient("bottom");
 
+// create yAxis
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left");
 
-
-
-// Add the x-axis.
-    
-
-
-// create left yAxis
-    var yAxisLeft = d3.svg.axis()
-// Add the y-axis to the left
-    
-
+//Create entire chart function
 function render(data){
     xScale.domain( d3.extent(data, function (d) { return d[0]; }));
     yScale.domain( d3.extent(data, function (d) { return d[1]; }));
 
-
     path.attr("d", line(data));
-    axis.call(xAxis);
+
+    xAxisG.call(xAxis);
+    yAxisG.call(yAxis);
 
 }
 

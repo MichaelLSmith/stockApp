dbe97773c89384df70a3308d883e9daae588fccd
MichaelLSmith <mlsmith45@gmail.com>
2016-01-05 18:29:37 -0500

    calling function from HTML

--- js/api.js
+++ js/api.js
@@ -1,40 +1,27 @@
-//ajax function from instagramAPI project 
 
-//function ajax_call(tag){
 
-//     // var photos={};
-//     $.ajax({
-//         url: 'https://www.quandl.com/api/v3/datasets/FRED/USARGDPQDSNAQ.json',
-//         type:'GET',
-//         // data:{clientId:'61f8b631abd34732a3bcd8c73d0d73a9'},
-//         dataType:'json',
+//Passing a value from HTML into a function: http://stackoverflow.com/questions/7957039/passing-value-from-text-input-into-javascript-function
 
-//         success:function(data){
-//             /* PLACE FUNCTION HERE */
-//             photos = data;
-//             console.log(photos);
-//             // images_response(photos);
-//             getData(data);
+function testFunction(){
+    var input = document.getElementById('stockUrl'),
+        stockUrl = input.value;
+    if (stockUrl) {
+        console.log(stockUrl);
+    }
+}
 
-            
-//         },
-//         error:function(data){
-//             console.log(data);
-//         }
-//     });
+
+// function ajax_call(url){
+//     console.log('ajax_call');
+//     apiCall = new XMLHttpRequest();
+//     apiCall.onreadystatechange = function(){
+//         if( apiCall.readystate == 4 && apiCall.status == 200)
+//             { console.log(apiCall.responseText); };
+//     };
+
+// //parameters of apiCall(method, url of api, async:true/false)
+// //for reference: http://www.w3schools.com/ajax/ajax_xmlhttprequest_send.asp
+//     apiCall.open("GET", url, true);
+//     apiCall.send();
 
 // }
-
-function ajax_call(url){
-    apiCall = new XMLHttpRequest();
-    apiCall.onreadystatechange = function{
-        if( apiCall.readystate == 4 && apiCall.status == 200)
-            { console.log(apiCall.responseText); };
-    };
-
-//parameters of apiCall(method, url of api, async:true/false)
-//for reference: http://www.w3schools.com/ajax/ajax_xmlhttprequest_send.asp
-    apiCall.open("GET", url, true);
-    apiCall.send();
-
-}

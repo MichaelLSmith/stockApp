41d76202bacbc454924bcdb92c2e1698e07221c0
MichaelLSmith <mlsmith45@gmail.com>
2016-01-07 13:08:28 -0500

    getting chart based on url

--- js/api.js
+++ js/api.js
@@ -1,51 +1,20 @@
-
+var stockUrl;
+var auth_url;
 
 //Passing a value from HTML into a function: http://stackoverflow.com/questions/7957039/passing-value-from-text-input-into-javascript-function
 
-// function testFunction(){
-//     var input = document.getElementById('stockUrl'),
-//         stockUrl = input.value;
-//     if (stockUrl) {
-//         console.log(stockUrl);
-//     }
-// }
-
-
-// function ajaxCall(){
-//     console.log('ajaxCall');
-//     var input = document.getElementById('stockUrl'),
-//         stockUrl = input.value;
-//     console.log(stockUrl);
-//     if (stockUrl) {
-
-//         console.log('if statement');
-
-//         apiCall = new XMLHttpRequest();
-//         apiCall.onreadystatechange = function(){
-//             console.log('onreadystatechange');
-//             if( apiCall.readystate == 4 && apiCall.status == 200)
-//                 { console.log(apiCall.responseText); }
-//             else {
-//                 console.log('Error: '+ apiCall.readystate);
-//             }
-//         };
-//     }
-// //parameters of apiCall(method, url of api, async:true/false)
-// //for reference: http://www.w3schools.com/ajax/ajax_xmlhttprequest_send.asp
-//     console.log('apiCall.open');
-//     apiCall.open("GET", 'https://www.quandl.com/api/v3/datasets/WIKI/WRB.json?auth_token=SQ6hUVEtaezYxrtDK__i', true);
-//     apiCall.send();
-
-// }
+function buildUrl(){
+    var input = document.getElementById('stockUrl'),
+        stockUrl = input.value;
+    if (stockUrl) {
+        auth_url = stockUrl + '?auth_token=SQ6hUVEtaezYxrtDK__i'
+    }
+    console.log(auth_url);
+}
 
 //url for W.R. Berkley Corp
 //https://www.quandl.com/api/v3/datasets/WIKI/WRB.json?auth_token=SQ6hUVEtaezYxrtDK__i
 
-
-//simple AJAX test
-
-
-// var stockObj;
 
 var request;
 
@@ -53,7 +22,8 @@ var request;
 function simpleAjax(){
     request = new XMLHttpRequest()
     console.log(request);
-    request.open('GET', 'https://www.quandl.com/api/v3/datasets/WIKI/ALEX.json?auth_token=SQ6hUVEtaezYxrtDK__i');
+    request.open('GET', auth_url);
+    console.log(auth_url);
     request.responseType = 'json';
 
     request.onreadystatechange = function() {

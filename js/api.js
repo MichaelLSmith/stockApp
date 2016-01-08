a07ff90297d770b531ccc3bcb2639165124e442e
MichaelLSmith <mlsmith45@gmail.com>
2016-01-08 17:45:44 -0500

    have search results filter companies

--- js/api.js
+++ js/api.js
@@ -1,9 +1,17 @@
 var auth_url;
 var base_url = 'https://www.quandl.com/api/v3/datasets/';
+var companyCode;
 
 //Passing a value from HTML into a function: http://stackoverflow.com/questions/7957039/passing-value-from-text-input-into-javascript-function
 
 function buildUrl(){
+    console.log('buildUrl');
+
+    //need to pass company code into this function from codesObj search results.
+    console.log
+
+
+
     var input = document.getElementById('stockCode'),
         stockCode = input.value;
     if (stockCode) {
@@ -41,21 +49,41 @@ function apiCall(){
 
 //   WIKI/CSII
 
+//Search Functionality
 
-//use d3 to parse csv of stock Codes
+    //use d3 to parse csv of stock Codes
+    //from http://bl.ocks.org/zross/6702090
 
-//creates and array of arrays
-d3.text('../stockList/WIKI_tickers.csv', function(data){
-    arrayOfCodes = d3.csv.parseRows(data);
-    // console.log(arrayOfCodes);
-});
+    //creates an array of objects
+    d3.csv('../stockList/WIKI_tickers.csv', function(data){
+        codesObj=data;
+        console.log(codesObj);
+        });
 
-//creates an array of objects
-d3.csv('../stockList/WIKI_tickers.csv', function(data){
-    codesObj=data;
-    // console.log(codesObj);
-    });
+//get data from parsed csv (either array or obj form, prob obj)
+
+function buildResults (codesObj) {
+    var input = document.getElementById("codeSearch");
+    // console.log(input.value);
+
+    // console.log(codesObj[0].name);
+    // console.log(codesObj[0].quandlcode);
+
+    var myExp = new RegExp(input.value, 'i');
+
+    var output = '<ul class=searchResults>';
 
 
+    codesObj.forEach(function(company, i){
+        // console.log(company);
+        if (company.name.search(myExp) != -1 ){
+            // console.log(company.name + company.quandlcode);
+        output += '<li><span onclick="buildUrl()">'+company.name+'</span></li>'
+        }
+    })
 
+    output += '</ul>';
 
+    //to output search results as HTML
+    document.getElementById("searchResultsBase").innerHTML = output;
+}
